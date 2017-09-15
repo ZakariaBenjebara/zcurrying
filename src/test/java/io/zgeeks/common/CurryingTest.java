62cc0b964dcf038f3b6b22841724f29790781e50
Zakaria Benjebara <zakaria.benjebara@nestle.com>
2017-09-15 17:57:44 +0100

    Add more unit testing

--- src/test/java/io/zgeeks/common/CurryingTest.java
+++ src/test/java/io/zgeeks/common/CurryingTest.java
@@ -12,4 +12,10 @@ public class CurryingTest {
         double result = Currying.<Double, Integer, Double>from((u, d) -> scalb(u, d)).apply(() -> new Tuple2(1.2, 2));
         Assert.assertEquals(4.8, result, 0);
     }
+
+    @Test
+    public void test_currying_with_three_parameters() {
+        String result = Currying.<Integer, Integer, Double, String>from(u1 -> u2 -> u3 -> Integer.toString(u1 + (int) scalb(u3, u2))).apply(() -> new Tuple3(2, 5, 1.2));
+        Assert.assertEquals("40", result);
+    }
 }
